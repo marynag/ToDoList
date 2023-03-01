de2481c26e16591b46b8cd68e1cb1dfae78bea6c
marynag <mgrabchak1109@gmail.com>
2023-03-01 14:12:49 +0200

    edit project info

--- src/components/todoDetails/todoDetails.jsx
+++ src/components/todoDetails/todoDetails.jsx
@@ -1,27 +1,25 @@
 import './todoDetails.css';
 
 export const TodoDetails = (props) => {
+	if (!props.open) return null;
 	const handleClick = () => {
 		props.setOpen(false);
 	};
+
 	return (
-		<>
-			{props.open && (
-				<div className={'modal'}>
-					<h2>
-						<b>{props.item.title}</b>
-					</h2>
-					<p>
-						<b>Description</b>
-					</p>
-					<p>{props.item.description}</p>
-					<div className={'status'}>
-						<p>Status: </p>
-						<input type='checkbox' name='scales' checked={props.item.status} />
-					</div>
-					<button onClick={handleClick}>Close</button>
-				</div>
-			)}
-		</>
+		<div className={'modal'}>
+			<h2>
+				<b>{props.item.title}</b>
+			</h2>
+			<p>
+				<b>Description</b>
+			</p>
+			<p>{props.item.description}</p>
+			<div className={'status'}>
+				<p>Status: </p>
+				<input type='checkbox' name='scales' checked={props.item.status} />
+			</div>
+			<button onClick={handleClick}>Close</button>
+		</div>
 	);
 };
