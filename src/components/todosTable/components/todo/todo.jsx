d6b73529c462eb8003ff8c1e9757a48c64017b32
marynag <mgrabchak1109@gmail.com>
2023-03-02 21:42:35 +0200

    ability to change status

--- src/components/todosTable/components/todo/todo.jsx
+++ src/components/todosTable/components/todo/todo.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react';
 import { TodoDetails } from '../../../todoDetails/todoDetails';
+import { memo } from 'react';
+import { useDispatch } from 'react-redux';
+import { UPDATE_STATUS } from '../../../../store/actionTypes';
 
-export const Todo = ({ item }) => {
+export const Todo = memo(function Todo({ item }) {
 	const [open, setOpen] = useState(false);
+	const dispatch = useDispatch();
 
 	const handleOpen = () => setOpen(true);
 
+	const handleChange = (item) => {
+		dispatch({ type: UPDATE_STATUS, payload: item });
+	};
 	return (
 		<>
 			<TodoDetails open={open} item={item} setOpen={setOpen} />
@@ -14,9 +21,13 @@ export const Todo = ({ item }) => {
 				<td>{item.title}</td>
 				<td>{item.description}</td>
 				<td>
-					<input type='checkbox' checked={item.status} />
+					<input
+						type='checkbox'
+						checked={item.status}
+						onChange={() => handleChange(item)}
+					/>
 				</td>
 			</tr>
 		</>
 	);
-};
+});
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,17 @@
-import { ADD_TODOS } from './actionTypes';
+import { ADD_TODOS, UPDATE_STATUS } from './actionTypes';
 
 export const reducer = (state = [], action) => {
 	switch (action.type) {
 		case ADD_TODOS:
 			return [...state, action.payload];
+		case UPDATE_STATUS:
+			const updatedTodos = state.map((item) => {
+				if (item.id === action.payload.id) {
+					item.status = !item.status;
+				}
+				return item;
+			});
+			return updatedTodos;
 		default:
 			return state;
 	}
